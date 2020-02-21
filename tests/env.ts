928f10e70a2152f8ef3295b8be17af2c8fea98d4
Jean Soon <jeansoon@gmail.com>
2020-02-21 16:40:47 +0800

    update localnet environment setup

--- tests/env.ts
+++ tests/env.ts
@@ -1,9 +1,5 @@
 'use strict';
 
-/**
- * PLEASE GET THOSE AUTHORITY WALLETS FROM VAULTS
- */
-
 interface Node {
     connection: {
         url: string,
@@ -31,99 +27,57 @@ interface Node {
         provider: string,
         issuer: string,
         middleware: string,
-        feeCollector: string
+        feeCollector: string,
     },
     nonFungibleToken: {
         provider: string,
         issuer: string,
         middleware: string,
         feeCollector: string,
-        itemReceiver: string
     }
 };
 
 const localnet: Node = {
     connection: {
-        url: "https://127.0.0.1:26657",
+        url: "http://127.0.0.1:26657",
         timeout: 60000
     },
     trace: {
         silent: true,
         silentRpc: true
     },
-    chainId: "localnet",
+    chainId: "maxonrow-chain",
     name: "mxw",
-    airDrop: "",
+    airDrop: "gold damp garlic turn host harbor else bird wrestle quarter surround parrot fan naive burst effort impact hen aware step gym ribbon inform cost",
     kyc: {
-        provider: "",
-        issuer: "",
-        middleware: ""
+        provider: "foot increase such wave plunge athlete gentle figure hub reunion transfer marriage rude license champion monkey fan balcony position birth onion circle hint cool",
+        issuer: "brisk barrel dose panther slice album family math cup cute awesome mechanic pattern rack erupt enforce alcohol wolf boil autumn family avoid brother legal",
+        middleware: "belt world purchase stick spare one music suggest dentist kit globe save snack sauce liquid face undo select ethics choose august rhythm cycle crucial"
     },
     alias: {
-        provider: "",
-        issuer: "",
-        middleware: "",
-        feeCollector: ""
+        provider: "frost anchor admit engage minute pony half half rival fiction radio oven pizza siren state virtual fiscal urge clap dentist captain pink viable strike",
+        issuer: "have silly fat resemble radar viable evolve start side path later point wreck annual mesh shoulder attitude own label robot test gossip anxiety year",
+        middleware: "snap youth depart side mail panel category human popular theme cotton receive club sell rebel matrix box opinion host sand oak century chair silk",
+        feeCollector: "mxw123xwuat5h9x92w6vdtn4fl2l03t7d793qugxvc"
     },
     fungibleToken: {
-        provider: "",
-        issuer: "",
-        middleware: "",
-        feeCollector: ""
+        provider: "naive hire arctic injury camp twelve actor valid process voice return unusual glad hen ginger brisk clever solve toss expire type road blood green",
+        issuer: "wreck fiber slice novel nurse guess plate oven cotton life thought tape addict thank frown ready rival walk dish short solution work arena nurse",
+        middleware: "police toilet cupboard song blanket duty wrestle public bike cattle install page option spell scout crop pig answer access alarm gain fish absent pen",
+        feeCollector: "mxw173qf9y2ae0cx8y07ez6qsl9k2gs2l5955hfc7x"
     },
     nonFungibleToken: {
-        provider: "",
-        issuer: "",
-        middleware: "",
-        feeCollector: "",
-        itemReceiver: ""
+        provider: "language indoor mushroom gold motor genuine tower ripple baby journey where offer crumble chuckle velvet dizzy trigger owner mother screen panic question cliff dish",
+        issuer: "appear scale write grow tiger puppy trick kite exhibit distance target cliff coin silly because train matrix weather list chat stamp warfare hobby ocean",
+        middleware: "guard loop tell accuse village list prevent sea dolphin weapon own track spike venue gun blind carry hawk weapon track rain amazing author eagle",
+        feeCollector: "mxw1md4u2zxz2ne5vsf9t4uun7q2k0nc3ly5g22dne",
     }
 };
 
-const testnet: Node = {
-    connection: {
-        url: "https://testnet.maxonrow.com",
-        timeout: 60000
-    },
-    trace: {
-        silent: true,
-        silentRpc: true
-    },
-    chainId: "testnet",
-    name: "mxw",
-    airDrop: "",
-    kyc: {
-        provider: "",
-        issuer: "",
-        middleware: ""
-    },
-    alias: {
-        provider: "",
-        issuer: "",
-        middleware: "",
-        feeCollector: ""
-    },
-    fungibleToken: {
-        provider: "",
-        issuer: "",
-        middleware: "",
-        feeCollector: ""
-    },
-    nonFungibleToken: {
-        provider: "",
-        issuer: "",
-        middleware: "",
-        feeCollector: "",
-        itemReceiver: ""
-    }
-};
-
-const nodes: { [name: string]: Node } = { localnet, testnet };
-
-// const nodeProvider: Node = localnet;
-const nodeProvider: Node = testnet;
+const nodes: { [name: string]: Node } = { localnet };
+const nodeProvider: Node = localnet;
 
 export {
-    nodeProvider, nodes, localnet, testnet,
+    nodeProvider, nodes, localnet,
     Node
 };
