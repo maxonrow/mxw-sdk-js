222e05edc9bd9d847696e4fcc54a9aaa97968d3e
NardoLee <nardo.lee@actechmy.com>
2020-03-03 17:27:33 +0800

    merge update

--- dist/_version.d.ts
+++ dist/_version.d.ts
@@ -1,1 +1,1 @@
-export declare const version = "0.7.6";
+export declare const version = "0.8.0";
--- dist/_version.js
+++ dist/_version.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.version = "0.7.6";
+exports.version = "0.8.0";
 //# sourceMappingURL=_version.js.map
